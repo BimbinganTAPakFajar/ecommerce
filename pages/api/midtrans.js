c8e5feadc71413e77c24ec6a92ab0cb8c88c22b3
Melanchthon Butarbutar <Kyu-u>
2023-05-08 19:05:55 +0700

    remove zombie code

--- pages/api/midtrans.js
+++ pages/api/midtrans.js
@@ -1,5 +1,3 @@
-import axios from "axios";
-
 export default async function handler(req, res) {
   const midtransClient = require("midtrans-client");
   // Create Snap API instance
@@ -12,15 +10,6 @@ export default async function handler(req, res) {
   const data = req.body;
   console.log(data, "MIDTRANS DATA IN API ROUTE");
   try {
-    // const midtransres = await axios(`https://app.midtrans.com/snap/snap.js`, {
-    //   method: "POST",
-    //   data: data,
-    //   headers: {
-    //     Authorization: `Basic ${Buffer.from(
-    //       process.env.NEXT_PUBLIC_MIDTRANS_SERVER_KEY
-    //     ).toString("base64")}`,
-    //   },
-    // });
     await snap.createTransaction(data).then((transaction) => {
       // transaction token
       snapToken = transaction.token;
@@ -28,7 +17,6 @@ export default async function handler(req, res) {
     });
     if (req.method === "POST") {
       console.log("yes");
-      // res.status(200).json(midtransres.data);
       res.status(200).json({ snapToken });
     }
   } catch (err) {
